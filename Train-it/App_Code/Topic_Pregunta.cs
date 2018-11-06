d946846fac39e127e4964eda88b5bc3165c87425
Alejandra Álvarez <ale.alvarez97@hotmail.com>
2018-11-05 20:00:01 -0600

    Commit topics

---
+++ Train-it/App_Code/Topic_Pregunta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Topic_Pregunta
+/// </summary>
+public class Topic_Pregunta
+{
+    public Topic_Pregunta()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+    public static int Id { get; set; }
+    public static string Topic { get; set; }
+}
---
+++ Train-it/App_Code/Topic_Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Topic_Test
+/// </summary>
+public class Topic_Test
+{
+    public Topic_Test()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+    public static int Id { get; set; }
+    public static string Topic { get; set; }
+}
